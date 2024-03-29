f8b8f338763bccb42bfd7ee941f22655885108e8
kanishka <amila.payable@gmail.com>
2024-03-29 22:11:30 +0530

    Update .gitignore, .gitignore, compiler.xml, and 7 more files

--- app/src/main/java/com/aminogira/aminonightclock/MainActivity.java
+++ app/src/main/java/com/aminogira/aminonightclock/MainActivity.java
@@ -1,5 +1,7 @@
 package com.aminogira.aminonightclock;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +9,98 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.os.Handler;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.PowerManager;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+//import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView timeTextView;
+    private Handler handler;
+    private boolean isBlinking = false;
+    private PowerManager.WakeLock wakeLock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        // Fullscreen mode
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Force landscape orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        timeTextView = findViewById(R.id.timeTextView);
+        timeTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 200); // Set maximum font size
+
+        handler = new Handler();
+        updateClock();
+
+//        PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+//        wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK, "MyApp::MyWakelockTag");
+//        wakeLock.acquire();
+
+    }
+
+
+
+ 
+
+    private void updateClock() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                updateTime();
+                handler.postDelayed(this, 1000); // Update every second
+            }
+        }, 1000);
+
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//               // toggleBlinking();
+//                handler.postDelayed(this, 60000); // Update every minute
+//            }
+//        }, 0);
+    }
+
+    private void updateTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        String currentTime = sdf.format(new Date());
+        timeTextView.setText(currentTime);
+    }
+
+    private void toggleBlinkingNotUse() {
+        if (isBlinking) {
+            timeTextView.setTextColor(getResources().getColor(android.R.color.darker_gray));
+        } else {
+            timeTextView.setTextColor(getResources().getColor(android.R.color.transparent));
+        }
+        isBlinking = !isBlinking;
     }
 }
